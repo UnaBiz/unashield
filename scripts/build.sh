ba6dff0710169a3c997e0b1088892bc707bfa05e
lupyuen-unabiz <ly.lee@unabiz.com>
2017-06-13 02:16:11 +0800

    Testing Jekyll PDF

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ cp ../patch/generator.rb /usr/local/lib/ruby/gems/2.4.0/gems/jekyll-pdf-0.1.6/li
 cp /mnt/c/Temp/document.rb /usr/local/lib/ruby/gems/2.4.0/gems/jekyll-pdf-0.1.6/lib/jekyll/pdf/
 
 # Delete generated site
-#rm -r _site
+rm -r _site
 
 # Needed to run github-pages in unsafe code for jekyll-pdf
 export DISABLE_WHITELIST=true
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # bundle install
 
+cp ../patch/generator.rb /usr/local/lib/ruby/gems/2.4.0/gems/jekyll-pdf-0.1.6/lib/jekyll/pdf/
 cp /mnt/c/Temp/document.rb /usr/local/lib/ruby/gems/2.4.0/gems/jekyll-pdf-0.1.6/lib/jekyll/pdf/
 
 # Delete generated site
-#rm -r _site
+rm -r _site
 
 # Needed to run github-pages in unsafe code for jekyll-pdf
 export DISABLE_WHITELIST=true
